830258b584c5347bf31ad1a9aabca02e35e58f63
Arman <armanebrahimikhoshdel@gmail.com>
2025-04-26 23:44:47 +0330

    Tile collision added

--- src/main/java/models/enums/TileType.java
+++ src/main/java/models/enums/TileType.java
@@ -1,7 +1,16 @@
 package models.enums;
 
 public enum TileType {
-    soil,
-    water,
-    grass;
+    soil(false),
+    water(true),
+    grass(false);
+    private boolean collisionOn;
+
+    TileType(boolean collisionOn) {
+        this.collisionOn = collisionOn;
+    }
+
+    public boolean isCollisionOn() {
+        return collisionOn;
+    }
 }
