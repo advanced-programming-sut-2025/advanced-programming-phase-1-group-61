de2fb1b27562c69dc846fe65d543729697f42cb7
Mahed <ghasdak2220@gmail.com>
2025-04-26 13:19:46 +0330

    cheat code regexes added

---
+++ src/main/java/models/enums/Commands/CheatCodes.java
@@ -0,0 +1,27 @@
+package models.enums.Commands;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public enum CheatCodes {
+    CHEAT_ADVANCE_TIME("^cheat\\s+advance\\s+time (?<hours>\\d+)h$"),
+    CHEAT_ADVANCE_DATE("^cheat\\s+advance\\s+date (?<date>\\d+)d$"),
+    CHEAT_THOR("^cheat\\s+Thor -l (?<x>\\d+)\\s+(?<y>\\d+))$"),
+    CHEAT_WEATHER_SET("^cheat\\s+weather\\s+set (?<type>.*)$"),
+    ENERGY_SET("^energy\\s+set\\s+-v (?<value>.*)$"),
+    ENERGY_UNLIMITED("^energy\\s+unlimited$"),
+    CHEAT_ADD_ITEM("^cheat\\s+add\\s+item\\s+-n (?<itemName>.*) -c (?<count>.*)$"),
+    CHEAT_SET_FRIENDSHIP("^cheat\\s+set\\s+friendship\\s+-n (?<animalName>.*) -c (?<amount>.*)$"),
+    CHEAT_ADD_MONEY("^cheat\\s+add (?<count>.*) dollars$");
+    private String pattern;
+    CheatCodes(String pattern) {
+        this.pattern = pattern;
+    }
+    public String getPattern() {
+        return pattern;
+    }
+    public Matcher getMatcher(String input) {
+        Matcher matcher = Pattern.compile(this.pattern).matcher(input);
+        return matcher.matches() ? matcher : null;
+    }
+}
