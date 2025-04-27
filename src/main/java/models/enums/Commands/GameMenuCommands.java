bd1aa9c503f5aa3164ec5ad4564972feb62b90ee
Mahed <ghasdak2220@gmail.com>
2025-04-27 23:19:32 +0330

    fixed????

--- src/main/java/models/enums/Commands/GameMenuCommands.java
+++ src/main/java/models/enums/Commands/GameMenuCommands.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum GameMenuCommands {
-    START_GAME("^start$"),
+    START_GAME("^game\\s+new\\s+-u(?:\\s+(\\S+))?(?:\\s+(\\S+))?(?:\\s+(\\S+))?$"),
     CHOOSE_MAP("^game\\s+map\\s+(?<number>\\d+)$"),
     EQUIP_TOOL("^tools\\s+equip (?<name>.*)$");
 
