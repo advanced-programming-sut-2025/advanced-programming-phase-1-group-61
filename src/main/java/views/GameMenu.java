ff5eaaccd74f2a39651228415015a0ffc0ce6800
Mahed <ghasdak2220@gmail.com>
2025-04-27 23:48:31 +0330

    fixed?

--- src/main/java/views/GameMenu.java
+++ src/main/java/views/GameMenu.java
@@ -1,46 +1,64 @@
 package views;
 
-import controllers.GameMenuController;
-import models.Result;
+import models.Game;
 import models.character.Character;
 import models.enums.Commands.GameMenuCommands;
 import models.enums.Commands.RegisterMenuCommands;
+import models.map.Map;
 import models.map.MapCreator.MapBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class GameMenu implements AppMenu{
     @Override
     public void check(Scanner scanner) {
-        String input = scanner.nextLine().trim();
+        String input = scanner.nextLine();
         Matcher start = GameMenuCommands.START_GAME.getMatcher(input);
         Matcher showCurrentMenu = RegisterMenuCommands.SHOW_CURRENT_MENU.getMatcher(input);
         Matcher equipTool=GameMenuCommands.EQUIP_TOOL.getMatcher(input);
-        Matcher showCurrentTool=GameMenuCommands.SHOW_CURRENT_TOOL.getMatcher(input);
-        Matcher showAvailableTools=GameMenuCommands.TOOLS_SHOW_AVAILABLE.getMatcher(input);
-        Matcher upgradeTool=GameMenuCommands.TOOLS_UPGRADE.getMatcher(input);
-        Matcher useTool=GameMenuCommands.TOOLS_USE.getMatcher(input);
+
 
         if(start!= null){
-            StringBuilder city = MapBuilder.buildFullMap(1,2,1,2);
-            System.out.println(city);
+
+            List<String> usernames = new ArrayList<>();
+            for (int i = 1; i <= 3; i++) {
+                String user = start.group(i);
+                if (user != null) {
+                    usernames.add(user);
+                }
+            }
+            System.out.println(usernames);
+
+            int[] mapNumber = new int[4];
+            int i = 0;
+            do{
+                System.out.println("choosing map for player number "+(i+1));
+                String mapChoosing = scanner.nextLine();
+                Matcher chooseMap = GameMenuCommands.CHOOSE_MAP.getMatcher(mapChoosing);
+                if(chooseMap != null){
+                    int number = Integer.parseInt(chooseMap.group("number"));
+                    if(number>2 || number <= 0){
+                        System.out.println("unavailable map number");
+                    }else {
+                        mapNumber[i] = number;
+                        i++;
+                    }
+                }else {
+                    System.out.println("you have to choose map using command: game map <number>");
+                }
+            }while (i<4);
+
+            Map map = MapBuilder.buildFullMap(mapNumber[0], mapNumber[1],mapNumber[2],mapNumber[3]);
+            System.out.println("map created successfully.");
         } else if (showCurrentMenu != null) {
             System.out.println("you are in game");
         }
         else if (equipTool != null) {
-            //character should be implemented for this part!
-//            Result result= GameMenuController.equipTool(equipTool,);
-//            System.out.println(result.message());
-        } else if (showCurrentTool!=null){
 
-        } else if(showAvailableTools!=null){
-
-        } else if(upgradeTool!=null){
-
-        } else if(useTool!=null) {
-
-        }else {
+        } else {
             System.out.println("invalid command");
         }
 
