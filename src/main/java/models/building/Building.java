a6e483af0828722f0ce9ff75e23eeee5de899df8
Alireza <alireza.moghaddas.4444@gmail.com>
2025-05-10 19:41:40 +0330

    adding animal and building list

--- src/main/java/models/building/Building.java
+++ src/main/java/models/building/Building.java
@@ -1,4 +1,26 @@
 package models.building;
 
+import models.character.Character;
+
 public class Building {
+    protected int space= 0;
+    protected String type;
+    protected String name;
+    public Building(String type, String name){
+        this.type = type;
+        this.name = name;
+    }
+    public String getType(){
+        return type;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getSpace(){
+        return space;
+    }
+    public boolean addInput(Object input){
+        return false;
+    }
+
 }
