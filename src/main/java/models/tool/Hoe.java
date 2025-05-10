db3f050adb6aa51d9cd8da41ee6b253005cc514c
Mahed <ghasdak2220@gmail.com>
2025-05-10 21:03:16 +0330

    conflict fixed?

--- src/main/java/models/tool/Hoe.java
+++ src/main/java/models/tool/Hoe.java
@@ -4,7 +4,9 @@ import models.enums.Direction;
 import models.enums.ToolType;
 
 public class Hoe extends Tool{
-
+    public Hoe() {
+        super(ToolType.Hoe);
+    }
     public String use(Direction direction){
         super.use(direction);
         return "used hoe!";
