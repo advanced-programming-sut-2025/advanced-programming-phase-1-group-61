6bba40d9111c70d632f95b4ea479d8ad1737115b
Arman <armanebrahimikhoshdel@gmail.com>
2025-05-10 14:34:09 +0330

    lvl and skill added

--- src/main/java/models/character/Skill/FishingSkill.java
+++
@@ -1,7 +0,0 @@
-package models.character.Skill;
-
-public class FishingSkill extends Skill {
-    public FishingSkill(int level, int experience) {
-        super(level, experience);
-    }
-}
--- src/main/java/models/character/Skill/MiningSkill.java
+++
@@ -1,7 +0,0 @@
-package models.character.Skill;
-
-public class MiningSkill extends Skill {
-    public MiningSkill(int level, int experience) {
-        super(level, experience);
-    }
-}
