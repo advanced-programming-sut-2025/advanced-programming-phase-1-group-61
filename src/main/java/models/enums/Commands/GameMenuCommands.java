34fd3747699e642f89d6dc029ee06621796394d6
Alireza <alireza.moghaddas.4444@gmail.com>
2025-05-11 22:56:12 +0330

    animal commands

--- src/main/java/models/enums/Commands/GameMenuCommands.java
+++ src/main/java/models/enums/Commands/GameMenuCommands.java
@@ -34,7 +34,7 @@ public enum GameMenuCommands {
     FEED_HEY("^feed\\s+hey\\s+-n\\s+(?<animal_name>.*)$"),
     PRODUCES("^produces$"),
     COLLECT_PRODUCES("^collect\\s+produces\\s+-n\\s+(?<animal_name>.*)$"),
-    SELL_ANIMAL("^sell\\s+animal\\s+-n\\s+(?<animal_name>.*)$");
+    SELL_ANIMAL ("^sell\\s+animal\\s+-n\\s+(?<animal_name>.*)$");
 
 
     private String pattern;
