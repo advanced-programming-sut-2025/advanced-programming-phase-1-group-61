fb6a5a85fab78c39baba661616dfbe2b25885b1e
Mahed <ghasdak2220@gmail.com>
2025-05-12 12:46:17 +0330

    pirre's general store added

---
+++ src/main/java/models/shops/ShopBackpacks.java
@@ -0,0 +1,28 @@
+package models.shops;
+
+import models.enums.BackpackType;
+
+public class ShopBackpacks {
+    private final BackpackType backpackType;
+    private final int price;
+    private final int limit;
+    private final String description;
+    public ShopBackpacks(BackpackType backpackType, int limit, int price, String description) {
+        this.backpackType = backpackType;
+        this.limit = limit;
+        this.price = price;
+        this.description = description;
+    }
+    public BackpackType getBackpackType() {
+        return backpackType;
+    }
+    public int getLimit() {
+        return limit;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+}
