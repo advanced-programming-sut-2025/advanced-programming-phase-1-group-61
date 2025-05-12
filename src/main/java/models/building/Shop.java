74e6c2b17318bfb3679f496b719f51be8dd2eec3
Mahed <ghasdak2220@gmail.com>
2025-05-12 13:02:16 +0330

    getCurrentShop in Character class needs to be implemented later!

--- src/main/java/models/building/Shop.java
+++ src/main/java/models/building/Shop.java
@@ -4,7 +4,12 @@ package models.building;
 import models.enums.ShopType;
 
 public class Shop extends Building {
-    public Shop(String type, String name , int X, int Y) {
+    private final ShopType shopType;
+    public Shop(String type, String name , int X, int Y,ShopType shopType) {
         super(String.valueOf(type), name, X, Y);
+        this.shopType = shopType;
+    }
+    public ShopType getShopType() {
+        return shopType;
     }
 }
