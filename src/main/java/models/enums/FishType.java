f54ea801c62456288669f6d672f2834d63fe385f
Arman <armanebrahimikhoshdel@gmail.com>
2025-05-12 11:32:01 +0330

    fishing completed

--- src/main/java/models/enums/FishType.java
+++ src/main/java/models/enums/FishType.java
@@ -1,33 +1,38 @@
 package models.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum FishType {
-    Salmon(Season.Fall,ItemType.Salmon),
-    Sardine(Season.Fall,ItemType.Sardine),
-    Shad(Season.Fall,ItemType.Shad),
-    BlueDiscus(Season.Fall,ItemType.Discus),
-    MidnightCarp(Season.Winter , ItemType.MidnightCarp),
-    Squid(Season.Winter , ItemType.Squid),
-    Tuna(Season.Winter , ItemType.Tuna),
-    Perch(Season.Winter , ItemType.Perch),
-    Flounder(Season.Spring , ItemType.Flounder),
-    Lionfish(Season.Spring , ItemType.LionFish),
-    Herring(Season.Spring , ItemType.Herring),
-    GhostFish(Season.Spring , ItemType.GhostFish),
-    Tilapia(Season.Summer , ItemType.Tilapia),
-    Dorado(Season.Summer , ItemType.Dorado),
-    Sunfish(Season.Summer,ItemType.Sunfish),
-    RainbowTrout(Season.Summer , ItemType.RainbowTrout),
-    Legend(Season.Spring , ItemType.Legend),
-    Glacierfish(Season.Winter , ItemType.Glacierfish),
-    Angler(Season.Fall , ItemType.Angler),
-    Crimsonfish(Season.Summer , ItemType.Crimsonfish)
+    Salmon(Season.Fall,ItemType.Salmon , 1),
+    Sardine(Season.Fall,ItemType.Sardine, 2),
+    Shad(Season.Fall,ItemType.Shad,3),
+    BlueDiscus(Season.Fall,ItemType.Discus,4),
+    MidnightCarp(Season.Winter , ItemType.MidnightCarp,11),
+    Squid(Season.Winter , ItemType.Squid,12),
+    Tuna(Season.Winter , ItemType.Tuna,13),
+    Perch(Season.Winter , ItemType.Perch,14),
+    Flounder(Season.Spring , ItemType.Flounder,21),
+    Lionfish(Season.Spring , ItemType.LionFish,22),
+    Herring(Season.Spring , ItemType.Herring,23),
+    GhostFish(Season.Spring , ItemType.GhostFish,24),
+    Tilapia(Season.Summer , ItemType.Tilapia,31),
+    Dorado(Season.Summer , ItemType.Dorado,32),
+    Sunfish(Season.Summer,ItemType.Sunfish,33),
+    RainbowTrout(Season.Summer , ItemType.RainbowTrout,34),
+    Legend(Season.Spring , ItemType.Legend,25),
+    Glacierfish(Season.Winter , ItemType.Glacierfish,15),
+    Angler(Season.Fall , ItemType.Angler,5),
+    Crimsonfish(Season.Summer , ItemType.Crimsonfish,35),
     ;
     private Season season;
     private ItemType fish;
+    private int fishId;
 
-    FishType(Season season, ItemType fish) {
+    FishType(Season season, ItemType fish,int id) {
         this.season = season;
         this.fish = fish;
+        this.fishId = id;
     }
 
     public Season getSeason() {
@@ -37,4 +42,19 @@ public enum FishType {
     public ItemType getFish() {
         return fish;
     }
+
+    public int getFishId() {
+        return fishId;
+    }
+
+    public static List<FishType> getFishTypeListBySeason(Season season){
+        List<FishType> fishTypes = new ArrayList<>();
+        for (FishType fishType : FishType.values()) {
+            if(fishType.getSeason().equals(season)){
+                fishTypes.add(fishType);
+            }
+        }
+        return fishTypes;
+    }
+
 }
--- src/main/java/models/tool/FishingPole.java
+++ src/main/java/models/tool/FishingPole.java
@@ -1,12 +1,15 @@
 package models.tool;
 
 import models.App;
+import models.Game;
+import models.RandomNumber;
 import models.character.Character;
-import models.enums.Direction;
-import models.enums.TileType;
-import models.enums.ToolType;
+import models.enums.*;
 import models.map.Map;
 import models.map.Tile;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class FishingPole extends Tool{
     public FishingPole() {
@@ -14,21 +17,74 @@ public class FishingPole extends Tool{
     }
 
     public String use(Direction direction){
-        Character character= App.getCurrentGame().getCurrentCharacter();
+        Game game =  App.getCurrentGame();
+        Character character= game.getCurrentCharacter();
         int targetX=character.getX()+direction.getDx();
         int targetY=character.getY()+direction.getDy();
-        Map map=App.getCurrentGame().getMap();
+        Map map=game.getMap();
         Tile tile = map.getTileByCordinate(targetX , targetY);
         if(targetY<0 || targetX<0 || targetY>=map.getHeightSize() || targetX>=map.getWidthSize())
             return "you cant fish void \n(pls stop trying to break our game)";
 
         if(!tile.getType().equals(TileType.Water)){
             return "what do you want to fish form here?! worms?";
-        }
-        if(character.getCurrentTool().getLevel().equals("educational")){
+        }else {
+            Season season =game.getDate().getSeason();
+            ItemType fish = null;
+            if(character.getCurrentTool().getLevel().equals("educational")){
+                switch (season){
+                    case Spring:{
+                        fish = FishType.Herring.getFish();
+                        break;
+                    }case Summer:{
+                        fish = FishType.Sunfish.getFish();
+                        break;
+                    }case Fall:{
+                        fish = FishType.Sardine.getFish();
+                        break;
+                    }case Winter:{
+                        fish = FishType.Perch.getFish();
+                        break;
+                    }
+                }
+                character.getInventory().addItem(fish , 1);
+            }else {
+               List<FishType> fishTypes = FishType.getFishTypeListBySeason(season);
+               fishTypes.sort(Comparator.comparing(fishType -> fishType.getFish().getPrice()));
+               int randNumber = RandomNumber.getRandomNumber() % 100;
+                if(randNumber < 30){
+                    fish = fishTypes.get(0).getFish();
+                } else if (randNumber < 40) {
+                    fish = fishTypes.get(1).getFish();
+                } else if (randNumber < 50) {
+                    fish = fishTypes.get(2).getFish();
+                } else if (randNumber < 60) {
+                    fish = fishTypes.get(3).getFish();
+                } else if (randNumber <65 && character.getSkill().getFishingLVL() >= 4) {
+                    fish = fishTypes.get(4).getFish();
+                }
+                if(fish != null){
+                    character.getInventory().addItem(fish , 1);
+                    character.getSkill().addFishingSkillXP(10);
+                    int dEnergy  = type.getEnergyConsumption(level)-character.getSkill().getFishingLVL();
+                    if(dEnergy <0){
+                        dEnergy = 0;
+                    }
+                    int newEnergy=character.getEnergy()-dEnergy;
+                    character.setEnergy(newEnergy);
+                }else {
+                    int dEnergy  = type.getEnergyConsumption(level)-character.getSkill().getFishingLVL();
+                    if(dEnergy <0){
+                        dEnergy = 0;
+                    }
+                    int newEnergy=character.getEnergy()-dEnergy;
+                    character.setEnergy(newEnergy);
+                    return "no fish better luck next time";
+                }
+            }
 
         }
-        return "Used fishing pole!";
+        return "Used fishing pole but something went wrong!";
     }
 
     @Override
