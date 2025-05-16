5e9d92dacf1aeb7ff141af101c844f6f79e4513e
Mahed <ghasdak2220@gmail.com>
2025-05-17 02:34:46 +0330

    ...

---
+++ src/main/java/models/shops/ShopRecipes.java
@@ -0,0 +1,39 @@
+package models.shops;
+
+import models.enums.Recipe;
+
+public class ShopRecipes {
+    private final Recipe recipe;
+    private final int limit;
+    private final int price;
+    private int stock;
+    private final String description;
+    public ShopRecipes(Recipe recipe, int limit, int price, String description) {
+        this.recipe = recipe;
+        this.limit = limit;
+        this.price = price;
+        this.description = description;
+        this.stock = limit;
+    }
+    public Recipe getRecipe() {
+        return recipe;
+    }
+    public int getLimit() {
+        return limit;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void restoreStock(){
+        stock=limit;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public String getDescription() {
+        return description;
+    }
+}
--- src/main/java/models/shops/StarDrop.java
+++ src/main/java/models/shops/StarDrop.java
@@ -2,12 +2,25 @@ package models.shops;
 
 
 import models.building.Shop;
+import models.enums.ItemType;
+import models.enums.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StarDrop extends Shop {
     //needs recipe
+    private final ArrayList<ShopItem> items;
+    private final ArrayList<ShopRecipes> recipes;
 
     public StarDrop( String name, int X, int Y) {
         super( name, X, Y);
+        this.items = new ArrayList<>(List.of(
+                new ShopItem(ItemType.Beer,Integer.MAX_VALUE,400,"Drink in moderation.")
+        ));
+        this.recipes = new ArrayList<>(List.of(
+
+        ));
     }
 
     @Override
