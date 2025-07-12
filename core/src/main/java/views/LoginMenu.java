86a7d8ba3143e2716c7f2f4420fd1f7b2995cd0d
cartaphilus06 <ghasdak2220@gmail.com>
2025-07-12 04:29:40 +0330

    added some shits

--- core/src/main/java/views/LoginMenu.java
+++ core/src/main/java/views/LoginMenu.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -12,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import controllers.LoginMenuController;
+import controllers.MainMenuController;
 import controllers.PreGameMenuController;
 import controllers.RegisterMenuController;
 import io.github.camera.Main;
@@ -28,23 +30,27 @@ public class LoginMenu implements Screen {
     private TextButton login;
     private TextButton forgetPassword;
     private TextButton registerMenu;
+    private TextButton backToMainMenu;
     private TextField username;
     private TextField password;
     private CheckBox stayLoggedIn;
     private String resultMessage = "";
     private BitmapFont font;
+    private Texture background;
 
     public LoginMenu(LoginMenuController controller) {
         this.controller = controller;
         stage = new Stage(new FitViewport(1920,1080));
         login = new TextButton("LOGIN",skin);
         forgetPassword = new TextButton("FORGET PASSWORD",skin);
+        backToMainMenu = new TextButton("MAIN MENU",skin);
         username = new TextField("",skin);
         username.setMessageText("USERNAME");
         password = new TextField("",skin);
         password.setMessageText("PASSWORD");
         stayLoggedIn = new CheckBox("stay logged in",skin);
         registerMenu = new TextButton("REGISTER MENU",skin);
+        background = AssetManager.getMainMenuBackground();
         controller.setView(this);
         font = AssetManager.getFont();
     }
@@ -68,6 +74,7 @@ public class LoginMenu implements Screen {
         buttonTable.add(forgetPassword).width(540).pad(20);
         buttonTable.add(login).width(240).pad(20);
         buttonTable.add(registerMenu).width(480).pad(20);
+        buttonTable.add(backToMainMenu).width(480).pad(20);
         buttonTable.bottom().padBottom(20);
         stage.addActor(buttonTable);
         Gdx.input.setInputProcessor(stage);
@@ -76,6 +83,7 @@ public class LoginMenu implements Screen {
         login.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
+                AssetManager.getUiClicks().play();
                 controller.setStayLoggedIn(stayLoggedIn.isChecked());
                 controller.setPassword(password.getText());
                 controller.setUsername(username.getText());
@@ -91,6 +99,7 @@ public class LoginMenu implements Screen {
         registerMenu.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
+                AssetManager.getUiClicks().play();
                 Main.getMain().getScreen().dispose();
                 Main.getMain().setScreen(new RegisterMenu(new RegisterMenuController()));
             }
@@ -98,8 +107,17 @@ public class LoginMenu implements Screen {
         forgetPassword.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
+                AssetManager.getUiClicks().play();
                 Main.getMain().getScreen().dispose();
                 Main.getMain().setScreen(new ForgetPasswordMenu());
+            }
+        });
+        backToMainMenu.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                AssetManager.getUiClicks().play();
+                Main.getMain().getScreen().dispose();
+                Main.getMain().setScreen(new MainMenu(new MainMenuController()));
             }
         });
     }
@@ -108,6 +126,7 @@ public class LoginMenu implements Screen {
     public void render(float v) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.getBatch().begin();
+        stage.getBatch().draw(background,0,0,stage.getViewport().getWorldWidth(),stage.getViewport().getWorldHeight());
         font.draw(stage.getBatch(), resultMessage, 50, stage.getViewport().getWorldHeight() - 50);
         stage.getBatch().end();
         stage.act(v);
